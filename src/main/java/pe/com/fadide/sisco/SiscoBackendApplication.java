e305183881587f47c70a171c79f6ea0df4107d1b
Walmer Gonzales <gonzaleswalmer0@gmail.com>
2026-09-09 17:01:46 -0500

    feat: inicializacion de proyecto base Spring Boot con dependencias

---
+++ src/main/java/pe/com/fadide/sisco/SiscoBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.com.fadide.sisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/fadide/sisco/SiscoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.fadide.sisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
